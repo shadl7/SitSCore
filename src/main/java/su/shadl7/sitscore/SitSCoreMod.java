55b2a73def342c604fe9a844f607d93026fe6b09
Shadl7 <tupjnin@gmail.com>
2024-07-31 22:37:31 +0700

    Require MixinBooter as hard dependency

--- src/main/java/su/shadl7/sitscore/SitSCoreMod.java
+++ src/main/java/su/shadl7/sitscore/SitSCoreMod.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import su.shadl7.sitscore.worldgen.WorldGenStartStruct;
 
-@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:mixinbooter")
 public class SitSCoreMod {
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_ID);
 
